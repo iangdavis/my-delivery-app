e1a1c594a79098ade798393f8a233e05e1a6b5ac
Ian Davis <70288333+iangdavis@users.noreply.github.com>
2026-08-18 21:38:07 -0500

    Show the delivery Live Activity widget

--- widget/widgetBundle.swift
+++ widget/widgetBundle.swift
@@ -1,17 +1,9 @@
-//
-//  widgetBundle.swift
-//  widget
-//
-//  Created by user268424 on 8/17/26.
-//
-
-import WidgetKit
 import SwiftUI
+import WidgetKit
 
 @main
 struct widgetBundle: WidgetBundle {
     var body: some Widget {
-        widgetLiveActivity()
+        DeliveryLiveActivity()
     }
 }
-
