13f90058e2b9de5e4d39f235951d38c85ff1799f
Ian Davis <70288333+iangdavis@users.noreply.github.com>
2026-08-19 13:13:17 -0500

    log APNs device token via AppDelegate so token prints to device logs

--- livehive/livehiveApp.swift
+++ livehive/livehiveApp.swift
@@ -6,9 +6,23 @@
 //
 
 import SwiftUI
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let token = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
+        print("APNs device token:", token)
+    }
+
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register for remote notifications:", error)
+    }
+}
 
 @main
 struct livehiveApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
